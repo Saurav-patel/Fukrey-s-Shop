1089ddca137604bbd4f4b93f6804c38de60d1240
Saurav-patel <khiladisaurav59@gmail.com>
2026-09-20 17:59:49 +0530

    Delivering

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+﻿
 document.addEventListener("DOMContentLoaded", () => {
 const menuToggle = document.querySelector(".menu-toggle");
     const mobileMenu = document.querySelector("#mobile-menu");
